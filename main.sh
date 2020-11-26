efd77f6a90e073570fd4e623ccd84dd7f960265e
Huntmix <73614708+huntmix@users.noreply.github.com>
2020-11-26 20:16:48 +0200

    Delete main.sh

--- main.sh
+++
@@ -1,7 +0,0 @@
-echo "Termux To Nethunter v1.0"
-echo "Установка нужных пакетов..."
-pkg install busybox tsu -y
-echo "Удаление предыдущего чртуа.."
-sudo rm -rf /data/local/nhsystem/kali-arm64
-echo "OK"
-wget https://images.kali.org/kalifs-arm64-minimal.tar.xz -O kali.tar.xz
